4af03fa7c2ee0712dd023b3afab987d71753c50c
Kesav Deepak Sridharan <139336019+KesavDeepak@users.noreply.github.com>
2025-10-19 17:40:16 +0530

    Time: 22 ms (26.40%) | Memory: 12.4 MB (77.20%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -5,9 +5,14 @@ class Solution(object):
         :rtype: int
         """
         
-        neg = -1 if x<0 else 1
+        if x<0:
+            neg = -1
+            x=-x
+        else:
+            neg = 1
+        
         r = 0
-        x = abs(x)
+        
         while x!=0:
             r = r*10+(x%10)
             x = x//10
